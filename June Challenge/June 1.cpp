6cecc6199510262fe4b3805608953218ff40271d
harshjain <harsh9500@rediffmail.com>
2020-06-01 20:08:24 +0530

    Added June 1.cpp

---
+++ June Challenge/June 1.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)
+            return nullptr;
+        queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* temp=q.front();
+            q.pop();
+            swap(temp->left,temp->right);
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+        return root;
+    }
+};
