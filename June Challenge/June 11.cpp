e36bd6583560e1c4a4b95a12afe092e17c256702
harshjain <harsh9500@rediffmail.com>
2020-06-11 12:56:58 +0530

    Added June 11.cpp

---
+++ June Challenge/June 11.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)
+            return;
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else
+            {
+                if(nums[mid]==2)
+                {
+                    swap(nums[mid],nums[high]);
+                    high--;
+                }
+                else
+                {
+                    mid++;
+                }
+            }
+        }
+    }
+};
