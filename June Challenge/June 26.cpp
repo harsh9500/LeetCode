c43d7fee9efc4298431af63003102ffdeae6e1f3
harshjain <harsh9500@rediffmail.com>
2020-06-26 15:21:44 +0530

    Added June 26.cpp

---
+++ June Challenge/June 26.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbersRecur(TreeNode * root, int sum)
+    {
+        if(root==nullptr)
+            return 0;
+        sum=(sum*10+root->val);
+        if(root->left==nullptr && root->right==nullptr)
+            return sum;
+        return sumNumbersRecur(root->left,sum)+sumNumbersRecur(root->right,sum);
+    }
+    int sumNumbers(TreeNode* root) {
+        return sumNumbersRecur(root,0);
+    }
+};
