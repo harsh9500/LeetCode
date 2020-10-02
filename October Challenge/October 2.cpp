b30537ab2fb77410ee7283b34638fe8f872b28f4
harshjain <harsh9500@rediffmail.com>
2020-10-02 14:10:29 +0530

    Added October 2.cpp

---
+++ October Challenge/October 2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void backtrack(vector<int> &candidates, int target, vector<vector<int> > &res, vector<int> &combination, int begin)
+    {
+        if (!target)
+        {
+            res.push_back(combination);
+            return;
+        }
+        for (int i=begin;i<candidates.size()&&target>=candidates[i];i++)
+        {
+            combination.push_back(candidates[i]);
+            backtrack(candidates,target-candidates[i],res,combination,i);
+            combination.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> res;
+        vector<int> combination;
+        backtrack(candidates,target,res,combination,0);
+        return res;
+    }
+};
