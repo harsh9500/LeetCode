7e31eefec1dcf2bc0b6fdfbdadfdb4e3ace4351f
harshjain <harsh9500@rediffmail.com>
2020-10-09 17:31:38 +0530

    Added October 9.cpp

---
+++ October Challenge/October 9.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string order;
+        inorderDFS(root, order);
+        return order;
+    }
+    
+    inline void inorderDFS(TreeNode* root, string& order)
+    {
+        if (!root)
+            return;
+        char buf[4];
+        memcpy(buf, &(root->val), sizeof(int));
+        for (int i=0; i<4; i++) order.push_back(buf[i]);
+        inorderDFS(root->left, order);
+        inorderDFS(root->right, order);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        int pos = 0;
+        return reconstruct(data, pos, INT_MIN, INT_MAX);
+    }
+    
+    inline TreeNode* reconstruct(const string& buffer, int& pos, int minValue, int maxValue)
+    {
+        if (pos >= buffer.size())
+            return NULL;
+        int value;
+        memcpy(&value, &buffer[pos], sizeof(int));
+        if (value < minValue || value > maxValue)
+            return NULL;
+        TreeNode* node = new TreeNode(value);
+        pos += sizeof(int);
+        node->left = reconstruct(buffer, pos, minValue, value);
+        node->right = reconstruct(buffer, pos, value, maxValue);
+        return node;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
