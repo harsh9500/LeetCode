51547c0cb6d464fc9e22284d4944f6dcb9b72f60
harshjain <harsh9500@rediffmail.com>
2020-10-14 22:56:38 +0530

    Added October 14.cpp

---
+++ October Challenge/October 14.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int robber(vector<int>& nums, int l, int r)
+    {
+        int pre=0, cur=0;
+        for (int i=l; i<=r;i++)
+        {
+            int temp=max(pre+nums[i],cur);
+            pre=cur;
+            cur=temp;
+        }
+        return cur;
+    }
+    
+    int rob(vector<int>& nums) {
+        int n=nums.size(); 
+        if (n<2)
+            return n?nums[0]:0;
+        return max(robber(nums,0,n-2),robber(nums,1,n-1));
+    }
+    
+};
