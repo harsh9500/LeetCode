3a8949b1e4fc89ffb3c9b3cbdbc10b53465ee242
harsh9500 <harsh9500@rediffmail.com>
2020-11-19 13:11:37 +0530

    Added November 17.cpp and November 18.cpp

---
+++ November Challenge/November 18.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+    vector<vector<int>> res;
+
+    sort(intervals.begin(), intervals.end());
+
+    res.push_back(intervals[0]);
+
+    for (int i=1;i<intervals.size();i++)
+    {
+
+        if (res.back()[1]<intervals[i][0]) res.push_back(intervals[i]);
+
+        else
+
+            res.back()[1]=max(res.back()[1],intervals[i][1]);
+
+    }
+
+    return res;
+    }
+};
