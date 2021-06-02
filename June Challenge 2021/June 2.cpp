bb4cee859a83501c301c5e2ad92d1f10c0025825
harsh9500 <harsh9500@rediffmail.com>
2021-06-02 20:48:14 +0530

    Added June 2.cpp

---
+++ June Challenge 2021/June 2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.length(), m = s2.length(), o = s3.length();
+        if (o != n + m)
+        {
+            return false;
+        }
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= m; j++)
+            {
+                if (i == 0 && j == 0)
+                {
+                    dp[i][j] = true;
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        dp[i][j] = dp[i][j - 1] && s2[j - 1] == s3[i + j - 1];
+                    }
+                    else
+                    {
+                        if (j == 0)
+                        {
+                            dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[i + j - 1];
+                        }
+                        else
+                        {
+                            dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) || (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
