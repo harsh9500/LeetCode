b25f84cb82a29702b7dd350d9d186cb0f3de2443
harsh9500 <harsh9500@rediffmail.com>
2021-06-11 12:51:59 +0530

    Added June 11.cpp

---
+++ June Challenge 2021/June 11.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int dp[1001][1001] = {};
+    
+    int dfs(vector<int>& s, int i, int j, int sum) {
+        if (i == j)
+            return 0;
+        return dp[i][j] ? dp[i][j] : dp[i][j] = max(sum - s[i] - dfs(s, i + 1, j, sum - s[i]),
+            sum - s[j] - dfs(s, i, j - 1, sum - s[j]));
+    }
+    
+    int stoneGameVII(vector<int>& stones) {
+        return dfs(stones, 0, stones.size() - 1, accumulate(begin(stones), end(stones), 0));
+    }
+};
