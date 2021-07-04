7228717bdade0e6bb35b1fa09a126897c30bb8fe
harsh9500 <harsh9500@rediffmail.com>
2021-07-04 18:14:52 +0530

    Added July 4.cpp

---
+++ July Challenge 2021/July 4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+        long aCount = 1, eCount = 1, iCount = 1, oCount = 1, uCount = 1;
+        int MOD = 1000000007;
+
+        for (int i = 1; i < n; i++) {
+            long aCountNew = (eCount + iCount + uCount) % MOD;
+            long eCountNew = (aCount + iCount) % MOD;
+            long iCountNew = (eCount + oCount) % MOD;
+            long oCountNew = (iCount) % MOD;
+            long uCountNew = (iCount + oCount) % MOD;
+            aCount = aCountNew;
+            eCount = eCountNew;
+            iCount = iCountNew;
+            oCount = oCountNew;
+            uCount = uCountNew;
+        }
+        long result = (aCount + eCount + iCount + oCount + uCount)  % MOD;
+        return result;
+    }
+};
